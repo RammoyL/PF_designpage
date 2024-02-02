51c74281087e895ddd15e84aaacdb3ef591d2952
Rammoy Lawrence <110222543+RammoyL@users.noreply.github.com>
2024-02-02 14:20:31 -0500

    Initial commit

---
+++ js/scrolltriggers.js
@@ -0,0 +1,29 @@
+const tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#parallax",
+        start: "top top",
+        end: "bottom top",
+        scrub: true
+    }
+});
+
+gsap.utils.toArray(".layer").forEach(layer => {
+    const speed = layer.dataset.speed;
+    const movement = -(layer.offsetHeight * speed)
+    tl.to(layer, {y:movement,ease: "none"},0)
+});
+
+let header = document.getElementById('header');
+
+document.addEventListener('scroll', function() {
+// Get scroll position
+let scrollPosition = window.pageYOffset;
+
+// Calculate whether the scroll it 350px or not then set the opacity to face the header out
+if (scrollPosition <= 150) {
+header.style.opacity = 1 - scrollPosition / 150;
+} else {
+header.style.opacity = 0;
+}
+
+});
